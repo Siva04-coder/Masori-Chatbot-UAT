c7f3dbf749170b029327868530b709c3a04f0d57
Siva04-coder <80594440+Siva04-coder@users.noreply.github.com>
2021-04-11 09:46:05 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -436,3 +436,5 @@ def refreshConsumerModel():
 # %% Main but it should be commented before upload to Git
 # if __name__ == "__main__":
 #     application.run(debug=True)
+import nltk
+nltk.download('punkt')
