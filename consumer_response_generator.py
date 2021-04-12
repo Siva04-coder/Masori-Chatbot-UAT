574e58f669f7ff0d829ba0b863bede5b61aaaa33
Siva04-coder <80594440+Siva04-coder@users.noreply.github.com>
2021-04-12 20:00:55 +0530

    Add files via upload

--- consumer_response_generator.py
+++ consumer_response_generator.py
@@ -40,7 +40,7 @@ class response_generator:
 
             if "I don't understand your question" in json_obj['output_text']:
                 response = response + '<div class="chat-text-divider"></div>'
-                response = response + '<a href="https://www.nuplazid.com/frequently-asked-questions">Click here to see FAQ</a>'
+                response = response + '<a href="https://nuplazid-masori.azurewebsites.net/frequently-asked-questions">Click here to see FAQ</a>'
             
             if 'Goodbye' in json_obj['output_text'] or 'My pleasure' in json_obj['output_text']:
                 response = response + '<div class="chat-individual-feedback"><span>Was this helpful?</span>'
@@ -123,7 +123,7 @@ class response_generator:
             if response == '':
                 response = "<p>I don't understand your question. Try asking the question in different way or ask me about something else.</p>"
                 response = response + '<div class="chat-text-divider"></div>'
-                response = response + '<a href="https://www.nuplazid.com/frequently-asked-questions">Click here to see FAQ</a>'
+                response = response + '<a href="https://nuplazid-masori.azurewebsites.net/frequently-asked-questions">Click here to see FAQ</a>'
             
         except Exception as e:
             print('Error')
