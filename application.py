07dad4650d4d781a2dbf05ef3ed76af995509dc4
Siva04-coder <80594440+Siva04-coder@users.noreply.github.com>
2021-05-05 10:15:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -154,8 +154,8 @@ def timeouthit():
     
     cur_response = ''
     cur_response = cur_response + "<p>Hi, I am SAM, the Search Assistant Manager. How can I help you today?</p>"
-    cur_response = cur_response + '<button class="chat-feedback-button-no" onclick="feedbacklookingno()">No</button>'
-    cur_response = cur_response + '<button class="chat-feedback-button-yes" onclick="feedbacklookingyes()">Yes</button></div>'
+    # cur_response = cur_response + '<button class="chat-feedback-button-no" onclick="feedbacklookingno()">No</button>'
+    # cur_response = cur_response + '<button class="chat-feedback-button-yes" onclick="feedbacklookingyes()">Yes</button></div>'
     #cur_response = consumer_geneset.feedback_generator(feedback)
 
     isAvoid = history.check_update_bot_history(uid, cur_response, disp_t)
